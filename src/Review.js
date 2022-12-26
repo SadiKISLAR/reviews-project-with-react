8ef84c4261f733eaf2754b5c244c0eb208556314
SadiKISLAR <sadi22kislar@gmail.com>
2022-12-26 10:19:26 +0300

    butttons bug solved

--- src/Review.js
+++ src/Review.js
@@ -6,17 +6,29 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0
+    }
+
+    if (number < 0) {
+      return people.length - 1
+    }
+    return number;
+  }
+
   const nextPerson = () => {
     setIndex((index) => {
       let newIndex = index + 1;
-      return newIndex;
+      return checkNumber(newIndex);
     })
   }
 
   const prevPerson = () => {
     setIndex((index) => {
-      let newIndex = index + - 1;
-      return newIndex;
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
     })
   }
 
